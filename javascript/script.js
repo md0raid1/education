9d48684706bae256f1c9402d73338f8fd4154727
md0raid1 <diclonius@live.ru>
2022-05-23 15:12:30 +0300

    start js d1

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+// alert('Я JavaScript')
+// let admin;
+// let AdminName = 'John';
+// admin = AdminName;
+// alert(admin)
+// let OurPlanetName;
+// let CurrentUserName;
+// let userName;
+// userName = 'Andrey';
+// alert(`hello ${1}`);
+// alert(`hello ${"userName"}`);
+// alert(`hello ${userName}`);
+// let userName = prompt('Как тебя зовут?');
+// alert(`Тебя зовут ${userName}`);
