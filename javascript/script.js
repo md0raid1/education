dad2b5fd551b4afc3f90ccdb47db4966c9083503
md0raid1 <diclonius@live.ru>
2022-05-26 11:18:09 +0300

    if-else-?

--- javascript/script.js
+++ javascript/script.js
@@ -12,3 +12,54 @@
 // alert(`hello ${userName}`);
 // let userName = prompt('Как тебя зовут?');
 // alert(`Тебя зовут ${userName}`);
+
+// JavaScript if/else constuction
+
+// let jsName = prompt('Какое "официальное" название JavaScript?', '');
+//   if (jsName != 'ECMAScript') {
+//     alert('Не знаете?\n"ECMAScript"!')
+//   }
+//   else {
+//     alert('Правильно!')
+//   }
+
+// let myNum = prompt('Введите число', '');
+// if (+myNum > 0) {
+//   alert(1)
+// }
+// else if (+myNum == 0) {
+//   alert(0)
+// }
+// else {
+//   alert(-1)
+// }
+
+// let result;
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много'
+// }
+
+// let result = (a + b < 4) ? 'Мало': 'Много';
+
+// let message;
+// if (login == 'Сотрудник') {
+// message = 'Привет';
+// } else if (login == 'Директор') {
+// message = 'Здравствуйте';
+// } else if (login == '') {
+// message = 'Нет логина';
+// } else {
+// message = '';
+// }
+
+// let login = prompt('Введите логин', '')
+// let message
+// login == 'Сотрудник' ? 
+//   message = ('Привет') :
+// login == 'Директор' ? 
+//   message = ('Здравствуйте') :
+// login == '' ? message = ('Нет логина') :
+//   message = ''
+// alert (message)
